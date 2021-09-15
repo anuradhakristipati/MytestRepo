f4d51cb839278714089c3fe2526dd6b2cfc756d9
anuradha kristipati <anuradha.kristipati@gmail.com>
2021-09-15 17:49:14 +0530

    New Program on Primitive Data types

---
+++ opeprogram1.java
@@ -0,0 +1,25 @@
+public class opeprogram1{
+public static void main(String[] args){
+	
+	int myInt = 20;
+	short myShort = 847;
+	long myLong = 979797;
+	float myFloat = 30.4f;
+	double myDouble = 40.40;
+	char myChar = 'A';
+	byte myByte = 17;
+	boolean myBoolean = false;
+
+		System.out.println(myInt);
+		System.out.println(myShort);
+		System.out.println(myLong);
+		System.out.println(myByte);
+		System.out.println(myFloat);
+		System.out.println(myDouble);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		
+		
+		
+		}
+}
